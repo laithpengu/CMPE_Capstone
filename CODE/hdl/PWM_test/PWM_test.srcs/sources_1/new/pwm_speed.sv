7d904643fda09abcd3335089eae025f0262a96df
Cole Cavanagh <103526842+laithpengu@users.noreply.github.com>
2024-02-07 20:49:26 -0500

    fixed pwm speed

--- CODE/hdl/PWM_test/PWM_test.srcs/sources_1/new/pwm_speed.sv
+++ CODE/hdl/PWM_test/PWM_test.srcs/sources_1/new/pwm_speed.sv
@@ -24,11 +24,11 @@ module pwm_speed(
     logic [7:0] data_in_d;
     logic data_out_d; 
     logic data_out_q;
-    logic [9:0] next_match_value_d;
-    logic [9:0] next_match_value_q; 
-    logic [9:0] curr_match_value_d;
-    logic [9:0] curr_match_value_q;
-    logic [9:0] counter;
+    logic [7:0] next_match_value_d;
+    logic [7:0] next_match_value_q; 
+    logic [7:0] curr_match_value_d;
+    logic [7:0] curr_match_value_q;
+    logic [7:0] counter;
 
     assign data_in_d = data_in;
     assign data_out = data_out_q;
@@ -37,9 +37,9 @@ module pwm_speed(
         if(rst) begin
             data_in_q <= 8'h00;
             data_out_q <= 1'b1;
-            counter <= 9'b0;
-            next_match_value_q <= 9'b0;
-            curr_match_value_q <= 9'b0;
+            counter <= 8'b0;
+            next_match_value_q <= 8'b0;
+            curr_match_value_q <= 8'b0;
         end else begin
             data_in_q <= data_in_d;
             data_out_q <= data_out_d;
@@ -51,12 +51,12 @@ module pwm_speed(
 
     always_comb begin
         // update next match value
-        next_match_value_d = ((data_in_q - 100) * 2) + 300;
+        next_match_value_d = data_in_q;
 
         if(counter == curr_match_value_q) begin // find match value
             data_out_d = 1'b0;
             curr_match_value_d = curr_match_value_q;
-        end else if(counter == 10'h25E) begin // invert on counter finish (@ 606)
+        end else if(counter == 8'hFF) begin // invert on counter finish (@ 256
             counter = 1'b0;
             data_out_d = 1'b1;
 
