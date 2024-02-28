eb19cbfec5cff88cf59f6995f50811bd4abf71a2
EthanSwain <103528060+EthanSwain@users.noreply.github.com>
2024-02-28 10:59:18 -0500

    Update RF_cl_test.sv

--- CODE/hdl/RF_cl_test/RF_cl_test.srcs/sources_1/new/RF_cl_test.sv
+++ CODE/hdl/RF_cl_test/RF_cl_test.srcs/sources_1/new/RF_cl_test.sv
@@ -162,7 +162,7 @@ always_comb begin
         end
     end
     idle_state: begin
-        inst_d = 2'b00;
+        //inst_d = 2'b00;
         cs_out_d=0;
         counter_d = 'b0;
         inc_d = 0;
@@ -305,6 +305,7 @@ always_comb begin
         Tx_ready_d = 'b1; // state machine is ready for control logic to transmit
         counter_d = 'b0;
             if(~intr && ready && Rx_ready) begin
+            //if(rst) begin
                 next_state = intr_read_1; 
                 Tx_ready_d = 'b0; //if an interupt is reacieved then state machine is busy  
         end else begin
@@ -338,12 +339,14 @@ always_comb begin
     end
     
     Rx_read_state: begin // this state reads from the RF fifo
-        if(Rx_counter_q < 8) begin // waits 8 fifo reads
+        if(Rx_counter_q < 'd8) begin // waits 8 fifo reads
             if(counter_q <= 'b1) begin // waits 2 cycles
                 inst_d = 'b10;// long read
                 addr_out_d = 'h300 + Rx_counter_q; // index the fifo 
                 counter_d = counter_q +1; // increase counter
+                next_state = Rx_read_state;
             end else begin
+                counter_d = 'b0;
                 next_state = Rx_read_reg;
                 cs_out_d = 'b1; // sends cs_out
             end
