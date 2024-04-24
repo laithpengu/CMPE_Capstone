e58afb92c047781b8887b78fd219356dab876621
hairjd2 <hairjd2@gmail.com>
2024-04-23 22:39:32 -0400

    pushing start

--- CODE/Arduino_code/proc/proc.ino
+++ CODE/Arduino_code/proc/proc.ino
@@ -5,10 +5,10 @@
 
 char ssid[] = "TP-Link_2F9C";
 char pass[] = "1cs_Pr0c";
-IPAddress ip(10, 0, 1, 16);
+IPAddress ip(192, 168, 0, 105);
 int status = WL_IDLE_STATUS;
 WiFiServer server_green(79);
-char server[] = "192.168.0.102";
+char server[] = "192.168.0.101";
 //char server[] = "10.0.1.16";
 String readString;
 bool connected = false;
@@ -33,12 +33,12 @@ mbed::PwmOut* pwmSpeed = new mbed::PwmOut(pinSpeed);
 int speed =0;
 int angle =0;
 
-
+char leader_ip[] = "";
+char follower_ip[] = "";
 
 ////////////////////////
 ///Motor control code///
 ////////////////////////
-
 
 // takes values between 60 and 120
 void vehicleAngle(int angle) {
@@ -56,24 +56,8 @@ void vehicleAngle(int angle) {
 
 // takes values between 0-255 (0 is stopped)
 void vehicleSpeed(int speed) {
-  /*
-  float scale = float(float(MAX_SPEED_PWM - MIN_SPEED_PWM) / float(MAX_SPEED - MIN_SPEED));
-  float offset = -MIN_SPEED * scale + MIN_SPEED_PWM;
-
-  int dutyPeriod = 0;
-  if (speed == 0) {
-    dutyPeriod = 0;
-  } else {
-    dutyPeriod = int(scale * speed) + offset;
-  }
-  pwmSpeed->pulsewidth_us(dutyPeriod);
-  delay(1);
-  Serial.println(dutyPeriod);
-  */
   analogWrite(pinSpeed,speed);
 }
-
-
 
 ///////////
 ///Setup///
@@ -101,8 +85,6 @@ void setup() {
 
   WiFi.config(ip);
   server_green.begin();
-
-
   Serial.print("SSID: ");
   Serial.println(WiFi.SSID());
   IPAddress ip = WiFi.localIP();
@@ -128,154 +110,76 @@ bool send(){
     return rv;
 }
 
+void receive() {
+  status = WiFi.status();
+  while (status != WL_CONNECTED) {
+      Serial.print("Attempting to connect to SSID: ");
+      Serial.println(ssid);
+      // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
+      status = WiFi.begin(ssid, pass);
 
-bool receive(){
-  bool rv = false;
-   WiFiClient client = server_green.available();
-  if (client) {
-    Serial.println("new client");
+      // wait 10 seconds for connection:
+      delay(2000);
+    }
+  while(!connected){
+    WiFiClient client = server_green.available();
+    if (client) {
+      connected = true;
+      Serial.println("new client");
 
-    while (client.connected())
-    {
-      if (client.available())
+      while (client.connected())
       {
-        char c = client.read();
-        if (readString.length() < 100)
+        if (client.available())
         {
-          readString += c;
-          Serial.write(c);
+          char c = client.read();
+          if (readString.length() < 100)
+          {
+            readString += c;
+            Serial.write(c);
 
-          if (c == '\n') {
-            if (readString.indexOf("?green") > 0)
-            {
-              int speedIndex = readString.indexOf("speed:") + 7; // Locate the start of the speed value
-              int angleIndex = readString.indexOf("angle:") + 7; // Locate the start of the angle value
-              
-              // Extract the speed value
-              String speedString = readString.substring(speedIndex, readString.indexOf(" ", speedIndex));
-              speed = speedString.toInt(); // Convert speed string to an integer
-              
-              // Extract the angle value
-              String angleString = readString.substring(angleIndex);
-              angle = angleString.toInt(); // Convert angle string to an integer
-              
-              // Output the extracted values
-              Serial.print("Speed: ");
-              Serial.println(speed);
-              Serial.print("Angle: ");
-              Serial.println(angle);
+            if (c == '\n') {
+              if (readString.indexOf("?green") > 0)
+              {
+                int speedIndex = readString.indexOf("speed:") + 7; // Locate the start of the speed value
+                int angleIndex = readString.indexOf("angle:") + 7; // Locate the start of the angle value
 
-              vehicleSpeed(speed);
-              vehicleAngle(angle);
+                // Extract the speed value
+                String speedString = readString.substring(speedIndex, readString.indexOf(" ", speedIndex));
+                speed = speedString.toInt(); // Convert speed string to an integer
+
+                // Extract the angle value
+                String angleString = readString.substring(angleIndex);
+                angle = angleString.toInt(); // Convert angle string to an integer
+
+                // Output the extracted values
+                Serial.print("Speed: ");
+                Serial.println(speed);
+                Serial.print("Angle: ");
+                Serial.println(angle);
+
+                vehicleAngle(angle);
+                vehicleSpeed(speed);
+                delay(1);
+              }
+
+              readString = "";
+
               delay(1);
-              rv = true;
+              // client.stop();
+              // Serial.println("client disconnected");
             }
-
-            readString = "";
-
-            delay(1);
-            // client.stop();
-            // Serial.println("client disconnected");
           }
         }
       }
     }
+    Serial.println("not recieved");
+    delay(500);
   }
-  return rv;
-}
-//////////
-///LOOP///
-//////////
-void loop() {
-  /*
-  while(!second_loop){
-    if (client.connect(server, 80)) {
-      second_loop = true;
-      Serial.println("connected");
-      client.println("GET /?green speed: 420 angle: 69 HTTP/1.0");
-      client.println();
-    }else{
-      Serial.println("Not connected");
-    }
-
-    //if (client.connected()) {
-    //  client.stop();
-    //}
-    delay(5000);
-  }
-  second_loop = false;
-  Serial.println("send loop left");
-  delay(5000);
-  */
-status = WiFi.status();
-while (status != WL_CONNECTED) {
-    Serial.print("Attempting to connect to SSID: ");
-    Serial.println(ssid);
-    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
-    status = WiFi.begin(ssid, pass);
-
-    // wait 10 seconds for connection:
-    delay(2000);
-  }
-while(!connected){
-  WiFiClient client = server_green.available();
-  if (client) {
-    connected = true;
-    Serial.println("new client");
-
-    while (client.connected())
-    {
-      if (client.available())
-      {
-        char c = client.read();
-        if (readString.length() < 100)
-        {
-          readString += c;
-          Serial.write(c);
-
-          if (c == '\n') {
-            if (readString.indexOf("?green") > 0)
-            {
-              int speedIndex = readString.indexOf("speed:") + 7; // Locate the start of the speed value
-              int angleIndex = readString.indexOf("angle:") + 7; // Locate the start of the angle value
-
-              // Extract the speed value
-              String speedString = readString.substring(speedIndex, readString.indexOf(" ", speedIndex));
-              speed = speedString.toInt(); // Convert speed string to an integer
-
-              // Extract the angle value
-              String angleString = readString.substring(angleIndex);
-              angle = angleString.toInt(); // Convert angle string to an integer
-
-              // Output the extracted values
-              Serial.print("Speed: ");
-              Serial.println(speed);
-              Serial.print("Angle: ");
-              Serial.println(angle);
-
-              vehicleAngle(angle);
-              vehicleSpeed(speed);
-              delay(1);
-            }
-
-            readString = "";
-
-            delay(1);
-            // client.stop();
-            // Serial.println("client disconnected");
-          }
-        }
-      }
-    }
-  }
-  Serial.println("not recieved");
-  delay(500);
-}
 
   Serial.println("recived loop left");
-connected = false;
-delay(500);
-while(!second_loop){
+  connected = false;
+  delay(500);
+  while(!second_loop) {
     if (client.connect(server, 80)) {
       second_loop = true;
       Serial.println("connected");
@@ -304,3 +208,29 @@ while(!second_loop){
   delay(500);
 }
 
+//////////
+///LOOP///
+//////////
+void loop() {
+  /*
+  while(!second_loop){
+    if (client.connect(server, 80)) {
+      second_loop = true;
+      Serial.println("connected");
+      client.println("GET /?green speed: 420 angle: 69 HTTP/1.0");
+      client.println();
+    }else{
+      Serial.println("Not connected");
+    }
+
+    //if (client.connected()) {
+    //  client.stop();
+    //}
+    delay(5000);
+  }
+  second_loop = false;
+  Serial.println("send loop left");
+  delay(5000);
+  */
+  receive();
+}
