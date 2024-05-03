371c79275d3eb67e216243df9d79b66644661943
Cole Cavanagh <103526842+laithpengu@users.noreply.github.com>
2024-05-03 15:24:18 -0400

    controller wifi updates

--- CODE/controllerArduino/wifiController/wifiController.ino
+++ CODE/controllerArduino/wifiController/wifiController.ino
@@ -1,22 +1,22 @@
 #include <SPI.h>
 #include <WiFiNINA.h>
 
-char ssid[] = "echo_router_24";
-char pass[] = "SouthRiver2020!";
-IPAddress ip(192, 168, 0, 101);
+char ssid[] = "TP-Link_2F9C";
+char pass[] = "1cs_Pr0c";
+IPAddress ip(192, 168, 0, 20);
 int status = WL_IDLE_STATUS;
-char server[] = "192.168.0.20";
 bool connected = false;
 
-String veh_ips[3] {"192.168.0.21", "192.168.0.22", "192.168.0.23"};
+char *veh_ips[3] {"192.168.0.21", "192.168.0.22", "192.168.0.23"};
 int leader_veh;
+const int veh_count = 3;
 
 WiFiClient client;
 
-const int pin_wheel = A0;
-const int pin_trigger = A1;
+const int pin_wheel = A1;
+const int pin_trigger = A0;
 
-enum {INIT, RUNNING} curr_state = INIT;
+enum {INIT, RUNNING} curr_state = RUNNING;
 
 ///////////
 ///Setup///
@@ -44,6 +44,8 @@ void setup() {
   }
   */
 
+  WiFi.config(ip);
+
   // attempt to connect to WiFi network:
   while (status != WL_CONNECTED) {
     Serial.print("Attempting to connect to SSID: ");
@@ -55,77 +57,79 @@ void setup() {
     delay(2000);
   }
 
-  WiFi.config(ip);
-
   Serial.print("SSID: ");
   Serial.println(WiFi.SSID());
   IPAddress ip = WiFi.localIP();
   IPAddress gateway = WiFi.gatewayIP();
   Serial.print("IP Address: ");
   Serial.println(ip);
-  Serial.println(WiFi.status());
 }
 
 //////////
 ///LOOP///
 //////////
 void loop() {
-  status - WiFi.status();
-  // attempt to connect to WiFi network:
-  while (status != WL_CONNECTED) {
-    Serial.print("Attempting to reconnect to SSID: ");
-    Serial.println(ssid);
-    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
-    status = WiFi.begin(ssid, pass);
-    // wait 10 seconds for connection:
-    delay(2000);
-  }
-
+  // status = WiFi.status();
+  // // attempt to connect to WiFi network:
+  // while (status != WL_CONNECTED) {
+  //   Serial.print("Attempting to reconnect to SSID: ");
+  //   Serial.println(ssid);
+  //   // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
+  //   status = WiFi.begin(ssid, pass);
+  //   // wait 10 seconds for connection:
+  //   delay(2000);
+  // }
+  // curr_state = RUNNING;
   if(curr_state == INIT) {
+    Serial.println("In INIT");
+    delay(10000);
     // set car order
     int order[3] = {2, 3, 1};
     leader_veh = order[0];
     // send init messages
-    int i = 0;
-    while (i < 3) {
-      if (client.connect(order[i], 80)) {
+    int i = veh_count - 1;
+    while (i >= 0) {
+      Serial.print("Attempting Connection: ");
+      Serial.print(veh_ips[order[i] - 1]);
+      Serial.println("...");
+      if (client.connect(veh_ips[order[i] - 1], 80)) {
         // define order for string
         char out_string[31];
         int leader = 0;
+        int follower = 17;
         if (i == 0) {
           leader = 1;
         }
-        int follower;
-        if (i == 2) {
-          follower = 17;
-        } else {
+        if (i != veh_count - 1) {
           follower = order[i + 1];
         }
 
         // set http
-        sprintf(out_string, "GET /?veh%d leader: %d follower %d", i + 1, leader, follower);
+        sprintf(out_string, "POST /?veh%d leader: %d follower: %d", order[i], leader, follower);
         Serial.println("Connection successfull. Sending: ");
         Serial.println(out_string);
         client.println(out_string);
         client.println();
+        //delay(500);
+        //client.println("POST /?kill");
         client.stop();
-        i++;
+        i--;
       } else {
         Serial.println("Connection failed. Attempting reconnection.");;
       }
     }
     curr_state = RUNNING;
+    delay(1000);
   } else if (curr_state == RUNNING) {
     // get motor data
     uint32_t wheel_val = analogRead(pin_wheel);
     uint8_t wheel_out;
-    if (wheel_val > 55 && wheel_val < 895) {
+    if (wheel_val > 50 && wheel_val < 895) {
       // convert to a binary
       // 115 - 185
-      Serial.println(wheel_val);
-      uint32_t OldRange = (900 - 55);
+      uint32_t OldRange = (900 - 50);
       uint32_t NewRange = (120 - 60);
-      wheel_out = (((wheel_val - 55) * NewRange) / OldRange) + 60;
+      wheel_out = (((wheel_val - 50) * NewRange) / OldRange) + 60;
       wheel_out = 120 - wheel_out + 60;
     } else {
       wheel_out = 90;
@@ -138,32 +142,36 @@ void loop() {
       //uint32_t int_trigger_val = (865+380) - trigger_val;
       //Serial.println(int_trigger_val);
       uint32_t OldRange = (865 - 395);
-      uint32_t NewRange = (255 - 0);
-      trigger_out = (((trigger_val - 395) * NewRange) / OldRange);
+      uint32_t NewRange = (50 - 0);
+      float scale = NewRange / OldRange;
+      int offset = int(-395 * scale);
+      trigger_out = int(trigger_val * scale) + offset;
+      //trigger_out = (((trigger_val - 395) * NewRange) / OldRange);
     } else {
       trigger_out = 0;
     }
+    Serial.println("trigger_out");
 
     for(int i = 0; i < 5; i++){
-      if (client.connect(server, 80)) {
+      if (client.connect(veh_ips[leader_veh - 1], 80)) {
         i = 5;
-        Serial.println("connected");
-        String message1 = "GET /?green speed: ";
+        char message1[18];
+        sprintf(message1, "POST /?veh%d speed: ", leader_veh);
         String message2 = " angle: ";
         String message3 = " HTTP/1.0";
         char speed_string [3];
         char angle_string [3];
         sprintf(speed_string, "%d", trigger_out);
         sprintf(angle_string, "%d", wheel_out);
-        String out_string = message1 + speed_string + message2 + angle_string + message3;
-        //String out_string = "GET /?green speed:420 angle: 69 HTTP/1.0";
+        String out_string = String(message1) + String(speed_string) + message2 + String(angle_string) + message3;
         Serial.println(out_string);
         client.println(out_string);
         client.println();
         client.stop();
+        break;
       }else{
         Serial.println("Not connected");
       }
     }
-  }
+  //}
 }
